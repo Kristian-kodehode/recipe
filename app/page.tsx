e4180620f2ea4a074a4a92c200feb91283491bfc
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-12-21 10:00:39 +0100

    updated tailwind config w colors

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Hello Recipe mini project</h1>
-      <h2>This will be fun</h2>
+      <h1 className="text-myPrimary">Hello Recipe mini project</h1>
+      <h2 className="text-mySecondary">This will be fun</h2>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,19 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      myPrimary: "#00805C",
+      mySecondary: "#004D37",
+      myBlack: "#262626",
+      myWhite: "#F5F5F5",
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
