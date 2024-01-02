4a42311377dbfa1def1bbf9ee07968eaedcfabb4
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-01-02 09:54:35 +0100

    trying to connect sanity to project

--- app/(Pages)/RecipePage/page.tsx
+++ app/(Pages)/RecipePage/page.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { client } from "@/app/lib/sanity";
 
-const RecipePage = () => {
+const getData = async () => {
+  const query = `*[_type == "recipe"]{
+    title, 
+    instructions, 
+    description, 
+    image
+  }`;
+  const data = await client.fetch(query);
+  return data;
+};
+
+const RecipePage = async () => {
+  const data = await getData();
   return (
     <section className="h-screen overflow-auto pt-32 pb-32 flex flex-col gap-4">
-      <h1 className="text-myPrimary text-center p-4">Recipe Page Heading</h1>
+      <h1 className="text-myPrimary text-center p-4">{data.title}</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, dolores
         quod velit exercitationem atque architecto, cupiditate repellat iusto
