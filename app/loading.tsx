f6c84bec794b4a1babaf0d6cdd32879662de6339
Kristian Kodehode <kristian007.kodehode@gmail.com>
2024-01-03 14:56:16 +0100

    added some lottie

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,12 @@
+"use client";
 import React from "react";
+import Lottie from "lottie-react";
+import animationData from "./assets/dataAnimation2.json";
 
 const Loading = () => {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <h1>Loading . . .</h1>
+      <Lottie animationData={animationData} />
     </div>
   );
 };
